178ed64157f02b59da4b40b546418b8f41c4f908
kaka1027 <maggie_yu0509@163.com>
2026-04-24 16:49:08 +0800

    部分游戏后端

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const path = require('path');
+const { Socket } = require('dgram');
+const { name } = require('ejs');
 
 const app = express();
 const server = http.createServer(app);
@@ -28,6 +30,38 @@ app.get('/report.html', (req, res) => {
   res.sendFile(path.join(__dirname, 'report.html'));
 });
 
+let players = [];
+let currentPlayerIndex = 0;
+let grid = Array(16).fill(null);
+let pool = [];
+let scores = {};
+
+const shape = ['circle', 'sqaure', 'star','triangle'];
+const color = ['red', 'blue', 'yellow', 'green'];
+
+function initPool(){
+  pool = [];
+  for (let s of shape){
+    for (let c of color){
+      pool.push({shape: s, color: c});
+    }
+  }
+}
+
+initPool();
+
+io.on('connection', (socket) => {
+  console.log('Player connection: ', socket.id);
+
+  socket.on('join', (socket) => {
+    if (!players.some( p => p.id === socket.id)) {
+      players.push({id: socket.id, name: name});
+    scores[name] = 0;
+    io.emit('playLists', players);
+    io.emit('scoreUpdate', scores);
+    }
+  })
+})
 
 // 启动
 const PORT = 8080;
